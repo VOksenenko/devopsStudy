a8c65a581dd4f5f21e8557869d9270f40453c89f
Vlad Oksenenko <voksenenko@gmail.com>
2021-06-28 21:54:42 +0000

    added switch depending on OS version

--- install_software.sh
+++ install_software.sh
@@ -1,7 +1,23 @@
 #!/bin/bash
 input=$1
+OS=$( cat /etc/os-release|grep -iE '^ID='| awk -F '=' '{print $2}'| tr -d '"' )
+
 while IFS= read -r line
 do
-  echo "$line"
-done < "$input"
+    case $OS in
+        ubuntu)
+            echo "ubuntu"
+            #yes | sudo apt-get install $line
+            yes| sudo apt remove $line
+            ;;
 
+        centos)
+            echo "centos"
+            sudo yum -y install $line
+            ;;
+
+       *)
+           echo  "Sorry. Cannot define OS distribution. Cannot install soft on this host."
+           ;;
+    esac
+done < "$input"
